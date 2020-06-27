b3149461ff520783b7e8181d52c30f777b4c2446
goldblackk <45857590+goldblackk@users.noreply.github.com>
2020-06-27 18:23:24 +0200

    Add files via upload

---
+++ fivem_ip_grabber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace fivem_ip_grabber
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Fivem IP Grabber by goldblack";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Give me server url address: ");
+            string urlinput = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Getting server ip address...");
+            Thread.Sleep(3000);
+            getIP(urlinput);
+        }
+        static void getIP(string urlinput)
+        {
+            var url = "https://" + urlinput;
+            Uri myUri = new Uri(url);
+            var ip = Dns.GetHostAddresses(myUri.Host)[0];
+            var port = myUri.Port;
+
+            Console.WriteLine();
+            Console.WriteLine("Server ip address: " + ip + port);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ fivem_ip_grabber/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("fivem_ip_grabber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fivem_ip_grabber")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("79bea85f-edf2-4407-9dd9-b7b4ce24236a")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
